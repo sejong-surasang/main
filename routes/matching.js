943bf38cdb0b869d7cf6aa1c7d0847a979c825d4
Gwanyeop Kim <81212131+cedric0708@users.noreply.github.com>
2022-09-06 17:14:01 +0900

    Update matching.js

--- routes/matching.js
+++ routes/matching.js
@@ -11,6 +11,22 @@ router.get('/', function(req,res,next){
   //res.send('shop 메인');
 });
 
+router.get('/matching', (req, res)=>{
+  res.sendFile(path.join(__dirname, '/matching.html'));
+});
+
+router.get('/matching/write', (req, res)=>{
+  res.sendFile(path.join(__dirname, '/matching/write.html'));
+});
+
+router.get('/matching/:id', (req, res)=>{
+  res.sendFile(path.join(__dirname, '/matching/:id.html'));
+});
+
+router.get('/matching/:id/revise', (req, res)=>{
+  res.sendFile(path.join(__dirname, '/matching/:id/revise.html'));
+});
+
 
 
 module.exports = router;
