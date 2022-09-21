4b267a0d64dc962bc49c376a682168d964093b45
seoo890 <31207606+seoo890@users.noreply.github.com>
2022-09-21 16:35:25 +0900

    feature : css, js파일 연결 설정 [#29]
    - file 라우터에서 sendfile 명령어 사용해서 파일 보낼 링크 설정.
    - html 파일의 링크 주소 수정

--- routes/file.js
+++ routes/file.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const path = require("path");
 
 const imgrouter = require('./img.js');
 const fontrouter = require('./font.js');
 
 router.use('/imgs', imgrouter);
 router.use('/font', fontrouter);
+
+router.get('/main/main.js', function(req,res,next){
+    res.sendFile(path.join(__dirname, "../views/main/", "main.js"));
+});
+router.get('/main/style.css', function(req,res,next){
+    res.sendFile(path.join(__dirname, "../views/main/", "style.css"));
+});
 
 module.exports = router;
