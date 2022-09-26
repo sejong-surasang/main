ad70fa8e38d3e40db3940e7ce94fb1a06c0a17a7
lwc61900 <lwc61900@naver.com>
2022-09-26 16:36:45 +0900

    feat : shop 라우터 추가 [#38]
    - shop.js 파일에 라우터 추가
    
    Resolves #38

--- routes/shop.js
+++ routes/shop.js
@@ -7,10 +7,19 @@ const DBconfig   = require('../config/sejongDB.js');
 const Connection = mysql.createConnection(DBconfig);
 
 router.get('/', function(req,res,next){
-  res.render('shop/index', {title:''});
-  //res.send('shop 메인');
+  
+  Connection.query('SELECT * from shop_lists', (error, rows) => {
+    if (error) throw error;
+     console.log('shop info is: ', rows);
+     res.render('shop/index', {title:'검수', rates:rows});
+   });
 });
 
+
+router.get('/:id', function(req, res, next) {
+  res.render('shop/index', {title:''});
+  res.send('shop 메인');
+});
 
 
 module.exports = router;
